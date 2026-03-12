ee5de3c2a5b005b8d931e7784481e41987815a8a
Ashutosh Ardak <ardakashutosh05@gmail.com>
2026-03-12 17:06:19 +0000

    add

--- backend/dynamodb.tf
+++ backend/dynamodb.tf
@@ -1,15 +1,15 @@
-# resource "aws_dynamodb_table" "terraform_lock" {
+resource "aws_dynamodb_table" "terraform_lock" {
 
-#   name         = "terraform-lock-table"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-#   tags = {
-#     Name = "Terraform Lock Table"
-#   }
-# }
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
